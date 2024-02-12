4ebd14a28b9c926e55b60f2dbffd39797c5ecffc
abimon <eddyabby59@gmail.com>
2024-02-12 18:06:22 +0300

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
         <div class="d-flex justify-content-center mt-3">
             <h3>The truth that changes hearts</h3>
         </div>
-        <button class="btn btn-outline-dark text-white rounded-pill shadow ps-5 pe-5 pt-2 pb-2" style="background-image: linear-gradient(to right,#4b5316,#411901)">Join Us</button>
+        <a href="{{route('register')}}"><button class="btn btn-outline-dark text-white rounded-pill shadow ps-5 pe-5 pt-2 pb-2" style="background-image: linear-gradient(to right,#4b5316,#411901)">Join Us</button>
     </div>
 </div>
 @endsection
