ef053ac6f86eed5a38cb01cf61765c98fe54527b
abimon <eddyabby59@gmail.com>
2024-06-21 07:29:14 +0300

    adjusted buttons

--- resources/views/Lessons/index.blade.php
+++ resources/views/Lessons/index.blade.php
@@ -45,7 +45,7 @@
             </div>
         </div>
     </div>
-    <div class="row d-flex justify-content-start">
+    <div class="row ">
         @foreach($courses as $k=>$course)
         <div class="col-md-4 p-2">
             <div class="card h-100">
