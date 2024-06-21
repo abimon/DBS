694a18187d30d4158659ab4125d0a5e608261a85
abimon <eddyabby59@gmail.com>
2024-06-21 07:50:20 +0300

    adjusted buttons

--- resources/views/Lessons/index.blade.php
+++ resources/views/Lessons/index.blade.php
@@ -49,7 +49,33 @@
         @foreach($courses as $k=>$course)
         <div class="col-md-4 p-2">
             <div class="card h-100">
-                
+                <img src="{{asset('storage/covers/'.$course->cover_path)}}" alt="Cover Image" style="height:30vh;object-fit:cover;">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <h5 class="card-title">{{$course->title}}</h5>
+                            <!-- <small>{{$course->category}}</small> -->
+                            <small class="text-secondary">
+                                <i class="bi bi-book-half"></i>
+                                {{$course->modules->count()}} Module(s)
+                            </small>
+                        </div>
+                        @if (Auth()->user()->role == "Admin")
+                        <div class="bi bi-people"> {{16*$k}}</div>
+                        @endif
+                    </div>
+                    <div class="card-text ">
+                        <?php echo html_entity_decode(mb_substr($course->description, 0, 250)); ?>
+                    </div>
+                    <div class="d-flex justify-content-between mt-3">
+                        <a href="/course/{{$k}}" class="btn bg-prim text-light">
+                            Enroll
+                        </a>
+                        <a href="{{route('module.index',['course'=>$course->id])}}" class="btn bg-prim text-light">
+                            Resume <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
         @endforeach
